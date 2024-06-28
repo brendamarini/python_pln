1761d62a3c0b6c87dd6d8ae1371896aba23c7a6b
brendamarini <112675066+brendamarini@users.noreply.github.com>
2024-06-28 19:19:41 -0300

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -1,33 +1,35 @@
-
+import sys
 import Levenshtein
 
 def carregar_perguntas(arquivo):
-  perguntas_respostas = {}
-  with open(arquivo, "r") as f:
-    for linha in f:
-      pergunta, resposta = linha.strip().split("|")
-      perguntas_respostas[pergunta.lower()] = resposta
-
-
-  return perguntas_respostas
+    perguntas_respostas = {}
+    with open(arquivo, "r") as f:
+        for linha in f:
+            pergunta, resposta = linha.strip().split("|")
+            perguntas_respostas[pergunta.lower()] = resposta
+    return perguntas_respostas
 
 def encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia=5):
-  menor_distancia = float("inf")
-  melhor_resposta = ""
-  for p, r in perguntas_respostas.items():
-    distancia = Levenshtein.distance(pergunta, p)
-    if distancia < menor_distancia:
-      menor_distancia = distancia
-      melhor_resposta = r
-  if menor_distancia <= limiar_distancia:
-      return melhor_resposta
-  else:
-      return "Pergunta não encontrada."
+    menor_distancia = float("inf")
+    melhor_resposta = ""
+    for p, r in perguntas_respostas.items():
+        distancia = Levenshtein.distance(pergunta, p)
+        if distancia < menor_distancia:
+            menor_distancia = distancia
+            melhor_resposta = r
+    if menor_distancia <= limiar_distancia:
+        return melhor_resposta
+    else:
+        return "Pergunta não encontrada."
 
 if __name__ == "__main__":
-  perguntas_respostas = carregar_perguntas("perguntas.txt")
-  limiar_distancia = 10
- 
-  pergunta = "quem é você"
-  resposta = encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia)
-  print("Resposta:", resposta)
+    if len(sys.argv) != 2:
+        print("Uso: python chat_bot.py <pergunta>")
+        sys.exit(1)
+
+    pergunta = sys.argv[1]
+    perguntas_respostas = carregar_perguntas("perguntas.txt")
+    limiar_distancia = 10
+
+    resposta = encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia)
+    print("Resposta:", resposta)
